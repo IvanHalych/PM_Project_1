3245e03b95206b6ce72842088309f1c90baf4b90
IvanHalych <39216494+IvanHalych@users.noreply.github.com>
2021-01-14 17:12:50 +0200

    Add project files.

---
+++ DrawingConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrawingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
